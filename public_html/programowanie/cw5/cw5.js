cf09812d49bc2460a36ea40990ec1c8265e854be
pszczesz <pszczesz@gmail.com>
2019-11-15 10:25:39 +0100

    d4

---
+++ public_html/programowanie/cw5/cw5.js
@@ -0,0 +1,28 @@
+window.onload = start;//wykonanie skryptu po zaladowaniu strony
+
+
+function start(){
+    const btn1 = document.querySelector("#btn1");
+    console.log(btn1);
+    const btn2 = document.querySelector("#btn2");
+    console.log(btn2);
+    const btn3 = document.querySelector("#btn3");
+    console.log(btn3);
+    //zdefiniowanie zdarzenia onclick dla przyciskow
+    btn1.onclick = klik1;
+    btn2.onclick = klik2;
+    btn3.onclick = klik3;
+}
+function klik1(){
+    console.log("kliknieto btn1");       
+    document.querySelector("#karta").style.backgroundColor = "red";
+    
+}   
+function klik2(){
+    console.log("kliknieto btn2");
+    document.querySelector("#karta").style.backgroundColor = "green";
+}
+function klik3(){
+    console.log("kliknieto btn3");
+    document.querySelector("#karta").style.backgroundColor = "blue";
+}
