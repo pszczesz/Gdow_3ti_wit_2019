77740608fd22b840b632b81b67a61ce4ad9e7cbc
pszczesz <pszczesz@gmail.com>
2019-11-26 10:18:35 +0100

    d3

---
+++ public_html/programowanie/cw7/cw7.js
@@ -0,0 +1,8 @@
+window.onload = start;
+
+function start(){
+    document.querySelector("#text").onkeyup = zmiana;
+}
+function zmiana(){    
+    document.querySelector("#wynik").innerHTML = this.value.toUpperCase();
+}
