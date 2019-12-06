5ca3fa0c7471ad05996a95ba145580702a5cb6b6
pszczesz <pszczesz@gmail.com>
2019-12-06 10:11:31 +0100

    d3

---
+++ public_html/programowanie/cw9/cw9.js
@@ -0,0 +1,19 @@
+window.onload = start;
+
+function start() {
+    document.querySelector("#prezent").onchange = sprawdz;
+
+}
+function sprawdz() {
+    if (czyGrzeczny()) {
+        document.querySelector("#wynik").innerText = "Niestety brak prezentu!!"
+            + this.value;;
+        return;
+    }
+    document.querySelector("#wynik").innerText = "Bardzo proszę oto prezent: "
+        + this.value;
+}
+function czyGrzeczny() {
+    const liczba = Math.floor(Math.random() * 10 + 1);
+    return liczba % 2 == 0 ? true : false;
+}
