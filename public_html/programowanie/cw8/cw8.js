b70165a915f02c836e1a0fc9dd0bb3a2f0730d36
pszczesz <pszczesz@gmail.com>
2019-12-06 09:57:22 +0100

    d3

--- public_html/programowanie/cw8/cw8.js
+++ public_html/programowanie/cw8/cw8.js
@@ -1,6 +1,5 @@
 window.onload = start;
 function start(){
-<<<<<<< HEAD
     document.querySelector("#wyswietl").onclick = wyswietl;
 }
 function wyswietl(){
@@ -8,15 +7,4 @@ function wyswietl(){
     const nazwisko = document.querySelector("#nazwisko").value;
     document.querySelector("#wynik").innerHTML = "Witaj "+imie+ " "+nazwisko+" dzisiaj mamy: "
        +(new Date().toLocaleDateString());
-=======
-    document.querySelector("#generuj").onclick = generuj;    
-}
-function generuj(){
-    const text = document.querySelector("#text").innerHTML;
-    let html = ""
-    for(let i=0;i<10;i++){
-        html += "<p>"+text+"</p>";
-    }
-    document.querySelector("#wynik").innerHTML = html;
->>>>>>> 10642c4d4e573e90622649ff0cab40fc460a6670
 }
