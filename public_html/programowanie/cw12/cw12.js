a9f249fa6ffa3e802ccd3d50b4bc903a65515955
pszczesz <pszczesz@gmail.com>
2020-01-10 10:02:45 +0100

    d4

--- public_html/programowanie/cw12/cw12.js
+++ public_html/programowanie/cw12/cw12.js
@@ -1,24 +1,31 @@
 window.onload = start;
 
-function start(){
+function start() {
     let vis = document.querySelector("#vis");
     let dis = document.querySelector("#dis");
     vis.onclick = visClick;
     dis.onclick = disClick;
+    document.querySelector("#btn1").onmousedown = function () {
+        this.setAttribute("src", "guzik3.png");
+    }
+    document.querySelector("#btn1").onmouseup = function () {
+        this.setAttribute("src", "guzik1.png");
+    }
+    document.querySelector("#btn1").onclick = visClick;
 }
-function visClick(){
+function visClick() {
     let scene = document.querySelector("#scene");
-    if(scene.style.visibility != "hidden"){
+    if (scene.style.visibility != "hidden") {
         scene.style.visibility = "hidden";
-    }else{
+    } else {
         scene.style.visibility = "visible";
-    }    
+    }
 }
-function disClick(){
+function disClick() {
     let scene = document.querySelector("#scene");
-    if(scene.style.display != "none"){
+    if (scene.style.display != "none") {
         scene.style.display = "none";
-    }else{
+    } else {
         scene.style.display = "block";
-    }    
+    }
 }
