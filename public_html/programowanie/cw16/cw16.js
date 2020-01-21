ae138e6289a7199161eb9ade1a5c6d7886de40f6
pszczesz <pszczesz@gmail.com>
2020-01-21 09:00:42 +0100

    d1

--- public_html/programowanie/cw16/cw16.js
+++ public_html/programowanie/cw16/cw16.js
@@ -1,7 +1,25 @@
 window.onload = function() {
   document.querySelector("#zegar").innerHTML = new Date().toLocaleTimeString();
+  setActualDate();
   setInterval(function() {
     let d = new Date();
     document.querySelector("#zegar").innerHTML = d.toLocaleTimeString();
   }, 1000);
 };
+function setActualDate(){
+    let months = ["Styczeń","Luty","Marzec","Kwiecień","Maj","Czerwiec","Lipiec",
+                "Sierpień","Wrzesień","Październik","Listopad","Grudzień"];
+    let days = ["Niedziela","Poniedziałek","Wtorek","Środa","Czwartek","Piątek",
+                "Sobota"];
+    let current = new Date();
+    let data = {month:months[current.getMonth()],
+                date:current.getDate(),
+                day:days[current.getDay()],
+                year: current.getFullYear()
+            };
+    console.log(data);
+    document.querySelector("#month").innerHTML = data.month;        
+    document.querySelector("#day").innerHTML = data.date;        
+    document.querySelector("#weekDay").innerHTML = data.day;        
+    document.querySelector("#year").innerHTML = data.year;        
+}
