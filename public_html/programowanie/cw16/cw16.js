f8292c67490ffbe8de25e97ff99592efaac617e2
pszczesz <pszczesz@gmail.com>
2020-01-21 08:08:26 +0100

    d1

--- public_html/programowanie/cw16/cw16.js
+++ public_html/programowanie/cw16/cw16.js
@@ -1,9 +1,7 @@
-window.onload = function(){
-    document.querySelector("#zegar")
-        .innerHTML = (new Date()).toLocaleTimeString();
-    setInterval(function(){
-        let d = new Date();
-        document.querySelector("#zegar")
-        .innerHTML = d.toLocaleTimeString();
-    },1000);
-}
+window.onload = function() {
+  document.querySelector("#zegar").innerHTML = new Date().toLocaleTimeString();
+  setInterval(function() {
+    let d = new Date();
+    document.querySelector("#zegar").innerHTML = d.toLocaleTimeString();
+  }, 1000);
+};
