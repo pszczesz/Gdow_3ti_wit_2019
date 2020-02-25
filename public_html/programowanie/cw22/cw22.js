c79fc47b419dfa0fdbff66aa11d8318df6ffc8db
pszczesz <pszczesz@gmail.com>
2020-02-25 12:02:31 +0100

    d3

--- public_html/programowanie/cw22/cw22.js
+++ public_html/programowanie/cw22/cw22.js
@@ -1,5 +1,6 @@
 window.onload = function(){
     let skarb = document.querySelector("#skarb");
+    let info = document.querySelector("#info");
     let size = 15;
     skarb.style.width = size+"%";
     skarb.style.height = size+"%";
@@ -9,12 +10,18 @@ window.onload = function(){
     
     skarb.style.left = left+"%";
     skarb.style.top = top+"%";
+    info.style.left = left+"%";
+    info.style.top = top+"%";
     console.log("left: "+left+" top: "+top);
     skarb.onmouseenter = function(){
         document.querySelector("#info")
            .innerHTML = "BRAWO znaleziono skarb!!!!";
+           document.querySelector("#skarb")
+           .style.backgroundColor = "green";
     }
     skarb.onmouseout = function(){
         document.querySelector("#info").innerHTML = "";
+        document.querySelector("#skarb")
+           .style.backgroundColor = "transparent";
     }
 }
